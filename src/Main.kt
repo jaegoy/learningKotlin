b718637b2faea69ab5daf6d62ff546d9815e866e
Jillian Goytia <jgoytia@schoology.com>
2018-03-23 19:43:49 -0400

    print even numbers

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,14 @@
 // This is main, the entry point of a Java/Kotlin Application
-fun main(args: Array<String>) {
-    // Sample Code stuffs
-    val firstNumber: Int = 123
-    var secondNumber: Int = 0
-    var sum = firstNumber + secondNumber
-    println("Sum of $firstNumber and $secondNumber = $sum")
-
-    // Change the numbers again:
-    secondNumber = 8
-    sum = firstNumber + secondNumber
-    println("Next Sum of $firstNumber and $secondNumber = $sum")
-
-    // Simple Prompt
-    print("What is your name? ")
-    // Reading Data from User:
-    val name: String? = readLine()
-    // Print the value of user input:
-    println("Your name is $name")
+fun mynameis (name: String): {
+    println("hello World $name")
 }
+
+//evennumbers
+fun printEven(start: Int, finish: Int) {
+    for (i in 4..30) {
+        if (i % 2 == 0)
+            println(i)
+    }
+}
+
+
