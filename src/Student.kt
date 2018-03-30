12420d546ff916b48930c0d439aec8a03fe10533
Unknown <jgoytia@schoology.com>
2018-03-30 14:42:52 -0400

    Teacher info

--- src/Student.kt
+++ src/Student.kt
@@ -1,12 +1,33 @@
-class Student(
-    val fistName: String,
-    val lastName: String,
-    val age: Int,
-    val gender: String,
-    val favoriteSubject: String,
-    val numeriGrade: Int)
-{
-    
+class Student1(
+    val fistName: "lucy",
+    val lastName: "love",
+    val age: 11,
+    val gender: "female",
+    val favoriteSubject: "reading",
+    val numeriGrade: 34)
+
+
+class Student2(
+        val fistName: "rick",
+        val lastName: "grimes",
+        val age: 13,
+        val gender: "male",
+        val favoriteSubject: "science",
+        val numeriGrade: 75)
+
+
+class Student3(
+        val fistName: "vima",
+        val lastName: "rome",
+        val age: 12,
+        val gender: "female",
+        val favoriteSubject: "history",
+        val numeriGrade: 90)
+
+
+{ fun getLetterGrade: String ()
+    //TODO add info
+return ()
 
 
 }
--- src/Teacher.kt
+++ src/Teacher.kt
@@ -1,3 +1,17 @@
-class Teacher {
+class Teacher(
+    val fistName: "Teacher",
+    val lastName: "J",
+    val age: 50,
+    val gender: "female",
+    val currentSubject: "religion",
+    val students: Array
+{ fun studentWithHighestGrade: String ()
+    //TODO add info
+    return ()
+
 
 }
+
+
+
+    }
